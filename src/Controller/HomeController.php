fa7d687d2d02157605119d02e6c452c95ae58743
el2mehdi1goual <elmehdigoual8@gmail.com>
2026-02-23 00:35:18 +0100

    Creation HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
+use App\Repository\FournisseurRepository;
+use App\Repository\MouvementstockRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +13,32 @@ use Symfony\Component\Routing\Annotation\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    #[Route('/', name: 'app_home_root')]
-    public function index(): Response
-    {
+    public function index(
+        ProduitRepository $produitRepo,
+        FournisseurRepository $fournisseurRepo,
+        MouvementStockRepository $mouvementRepo
+    ): Response {
         $this->denyAccessUnlessGranted('ROLE_USER');
+        $productCount = $produitRepo->count([]);
+        $supplierCount = $fournisseurRepo->count([]);
+        $movementCount = $mouvementRepo->count([]);
 
-        return $this->render('home/index.html.twig');
+        // Alertes = produits où stockActuel <= stockMin
+        $alertCount = $produitRepo->countLowStock();
+
+        return $this->render('home/index.html.twig', [
+            'productCount' => $productCount,
+            'supplierCount' => $supplierCount,
+            'movementCount' => $movementCount,
+            'alertCount' => $alertCount,
+        ]);
     }
+  
+#[Route('/stock', name: 'stock', methods: ['GET'])]
+public function stockRedirect(): Response
+{
+    return $this->redirectToRoute('stock_history');
+}
+
 }
 
